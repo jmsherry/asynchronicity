b80468b1956cf91c518cbe5cd95ff4446ae04f12
James Sherry <james.m.sherry@googlemail.com>
2022-06-03 16:43:10 +0100

    added flavour code to no-control

--- no-control-main.js
+++ no-control-main.js
@@ -4,7 +4,9 @@ import api3Call from './noControlAPIs/api3.js';
 
 // Bad. You've no guarantee that one will work before the other. This is especially bad if you need to pass the results of calling api1 to api2, for example
 console.log('sync calls');
-api1Call();
-api2Call();
-api3Call();
+const res1 = api1Call();
+console.log(`Trying to read the result of api1call synchonously: ${res1}`);
+const res2 = api2Call(res1);console.log(`Trying to read the result of api2call synchonously: ${res2}`);
+const finalRes = api3Call(res2);
+console.log(`Trying to read the result of api3call synchonously: ${finalRes}`);
 console.log('end sync calls');
--- noControlAPIs/api3.js
+++ noControlAPIs/api3.js
@@ -1,6 +1,6 @@
 import random from './../random.js';
 
-export default function fakeAPI2Call(value) {
+export default function fakeAPI3Call(value) {
   console.log('fakeAPI3Call executes');
   setTimeout(() => {
     console.log('fakeAPI3Call returns');
